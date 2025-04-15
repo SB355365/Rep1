b5d8d62f50736c4f5fcafb714956c1d84b7a7ff0
satya b <sb355365@gmail.com>
2025-04-15 16:28:45 +0000

    c2

--- models/INT/ORDERS_DELTA.sql
+++ models/INT/ORDERS_DELTA.sql
@@ -6,7 +6,10 @@
 
 }}
 
-select * from {{ref('Stg_Orders')}}
+
+
+
+--select * from {{ref('Stg_Orders')}}
 {% if is_incremental() %}
 where ORDER_DATE> (SELECT MAX(COALESCE(ORDER_DATE,CURRENT_DATE()-8))  FROM {{this}})
 {% endif %}
